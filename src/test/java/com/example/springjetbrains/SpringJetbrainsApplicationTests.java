ba0a6fce3c02a458d32c6190eb365cf308428520
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-05-28 12:38:08 +0600

    dependecies installed

---
+++ src/test/java/com/example/springjetbrains/SpringJetbrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjetbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJetbrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
