6b0f2d2096578c4347a46d3f071a11f044ca2c0e
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-06-08 02:33:47 +0600

    added swaggar documentation

--- backend/src/main/java/com/example/jdk17test/Jdk17testApplication.java
+++ backend/src/main/java/com/example/jdk17test/Jdk17testApplication.java
@@ -2,6 +2,9 @@ package com.example.jdk17test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class Jdk17testApplication {
