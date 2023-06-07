f5cf16cfe76c9131701a0024766618be7b565987
mehrab-haque <mehrab.haque.0001@gmail.com>
2023-06-08 02:38:07 +0600

    added swaggar documentation

--- backend/src/test/java/com/example/jdk17test/Jdk17testApplicationTests.java
+++ backend/src/test/java/com/example/jdk17test/Jdk17testApplicationTests.java
@@ -2,7 +2,6 @@ package com.example.jdk17test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
 class Jdk17testApplicationTests {
